9025e8aa728960a359b0837cf24e9af162bdce08
lmeyre <lmeyre@student.42.fr>
2021-09-19 04:28:57 +0200

    base du visuel, rotate, mais pas rotate segment individuels

---
+++ Assets/Imported/RotateBigCube.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBigCube : MonoBehaviour
+{
+    private Vector2 clickStartPos;
+    private Vector2 currentSwipe;
+    private Vector3 previousMousePosition;
+    private Vector3 mouseDelta;
+    float speed = 200f;
+    public GameObject target;
+
+    void Update()
+    {
+        Swipe();
+        Drag();
+    }
+
+    void Drag()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            // while the mouse is held down the cube can be moved around its central axis to provide visual feedback
+            mouseDelta = Input.mousePosition - previousMousePosition;
+            mouseDelta *= 0.1f; // reduction of rotation speed
+            transform.rotation = Quaternion.Euler(mouseDelta.y, -mouseDelta.x, 0) * transform.rotation;
+        }
+        else
+        {
+            // automatically move to the target position
+            if (transform.rotation != target.transform.rotation)
+            {
+                var step = speed * Time.deltaTime;
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, step);
+            }
+        }
+        previousMousePosition = Input.mousePosition;
+
+
+    }
+
+    void Swipe()
+    {
+        if (Input.GetMouseButtonDown(1))
+            clickStartPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        if (Input.GetMouseButtonUp(1))
+        {
+            Vector2 endPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            currentSwipe = new Vector2(endPos.x - clickStartPos.x, endPos.y - clickStartPos.y);
+            currentSwipe.Normalize();
+
+            if (LeftSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, 90, 0, Space.World);
+            }
+            else if (RightSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, -90, 0, Space.World);
+            }
+            else if (UpLeftSwipe(currentSwipe))
+            {
+                target.transform.Rotate(90, 0, 0, Space.World);
+            }
+            else if (UpRightSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, 0, -90, Space.World);
+            }
+            else if (DownLeftSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, 0, 90, Space.World);
+            }
+            else if (DownRightSwipe(currentSwipe))
+            {
+                target.transform.Rotate(-90, 0, 0, Space.World);
+            }
+        }
+    }
+
+    bool LeftSwipe(Vector2 swipe)
+    {
+        return currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
+    }
+
+    bool RightSwipe(Vector2 swipe)
+    {
+        return currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
+    }
+
+    bool UpLeftSwipe(Vector2 swipe)
+    {
+        return currentSwipe.y > 0 && currentSwipe.x < 0f;
+    }
+
+    bool UpRightSwipe(Vector2 swipe)
+    {
+        return currentSwipe.y > 0 && currentSwipe.x > 0f;
+    }
+
+    bool DownLeftSwipe(Vector2 swipe)
+    {
+        return currentSwipe.y < 0 && currentSwipe.x < 0f;
+    }
+
+    bool DownRightSwipe(Vector2 swipe)
+    {
+        return currentSwipe.y < 0 && currentSwipe.x > 0f;
+    }
+
+}
---
+++ Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+    [SerializeField] float speed = 200f;
+    Quaternion target;
+    bool isRotating = false;
+
+    void Awake()
+    {
+        target = transform.rotation;
+    }
+
+    void Update()
+    {
+        FollowTarget();
+        if (!isRotating)
+            CubeRotation();
+    }
+
+    void FollowTarget()
+    {
+        if (transform.rotation != target)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target, speed * Time.deltaTime);
+            isRotating = true;
+        }
+        else
+            isRotating = false;
+    }
+
+    void CubeRotation()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            target = Quaternion.Euler(0, 90, 0) * transform.rotation;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            target = Quaternion.Euler(0, -90, 0) * transform.rotation;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            target = Quaternion.Euler(-90, 0, 0) * transform.rotation;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            target = Quaternion.Euler(90, 0, 0) * transform.rotation;
+        }
+    }
+}
